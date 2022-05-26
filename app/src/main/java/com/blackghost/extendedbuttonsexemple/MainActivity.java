a93230dba5981519b8927a6ff4492c12fa80a22f
Michael Tsvetkov <blackghost0702@gmail.com>
2022-05-26 23:18:52 +0300

    v.1

--- app/src/main/java/com/blackghost/extendedbuttonsexemple/MainActivity.java
+++ app/src/main/java/com/blackghost/extendedbuttonsexemple/MainActivity.java
@@ -3,12 +3,25 @@ package com.blackghost.extendedbuttonsexemple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view) {
+        Log.e(TAG, "onClick");
+    }
+
+    public void onConfirmPressed(View view) {
+        Log.e(TAG, "onConfirmPressed");
+    }
+
 }
